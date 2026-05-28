4fcaa4d9f42b7a87577b5e596e0fafcc94f5187a
Will Mobley <feulmob+github@gmail.com>
2026-05-28 06:21:21 -0500

    Fix ClusterODM cleanup and split debug logging

--- libs/utils.js
+++ libs/utils.js
@@ -23,8 +23,6 @@ const async = require('async');
 const logger = require('./logger');
 const Readable = require('stream').Readable;
 const rimraf = require('rimraf');
-const child_process = require('child_process');
-const os = require('os');
 const path = require('path');
 const config = require('../config');
 
@@ -42,6 +40,28 @@ try{
 }
 
 const tmpUploadsMap = {}; // tmp dir entries --> number of files
+const tmpCleanupFailures = {}; // tmp dir entries --> last cleanup warning timestamp
+
+const NFS_BUSY_WARN_INTERVAL_MS = 1000 * 60 * 30;
+
+const shouldLogCleanupFailure = (dir) => {
+    const now = Date.now();
+    const last = tmpCleanupFailures[dir] || 0;
+    if (now - last >= NFS_BUSY_WARN_INTERVAL_MS) {
+        tmpCleanupFailures[dir] = now;
+        return true;
+    }
+    return false;
+};
+
+const hasNfsTempFiles = async (dir) => {
+    try {
+        const entries = await fs.promises.readdir(dir);
+        return entries.some(entry => entry.startsWith('.nfs'));
+    } catch (_) {
+        return false;
+    }
+};
 
 module.exports = {
 	get: function(scope, prop, defaultValue){
@@ -83,81 +103,104 @@ module.exports = {
 
         logger.info(`[TAPIS DEBUG] cleanupTemporaryDirectory called with timeout: ${staleUploadsTimeout}`);
 
-        return new Promise((resolve, reject) => {
-            fs.readdir(TMP_ROOT, async (err, entries) => {
-                if (err) reject(err);
-                else{
-                    for (let entry of entries){
-                        if (entry === '.gitignore') continue;
+        let entries;
+        try {
+            entries = await fs.promises.readdir(TMP_ROOT);
+        } catch (err) {
+            logger.error(err);
+            throw err;
+        }
 
-                        let stale = false;
-                        let tmpPath = path.join(TMP_ROOT, entry);
-                        logger.info(`[TAPIS DEBUG] Checking directory for cleanup: ${tmpPath}`);
+        for (let entry of entries){
+            if (entry === '.gitignore') continue;
 
-                        if (staleUploadsTimeout > 0){
-                            try{
-                                // Check if directory has a Tapis upload lock file
-                                const lockFile = path.join(tmpPath, '.tapis_upload_in_progress');
-                                if (fs.existsSync(lockFile)) {
-                                    logger.info(`[TAPIS DEBUG] Skipping cleanup of ${tmpPath} - Tapis upload in progress`);
-                                    continue;
-                                } else {
-                                    logger.info(`[TAPIS DEBUG] No lock file found at ${lockFile}, proceeding with cleanup check`);
-                                }
-                                
-                                const fileCount = await self.filesCount(tmpPath);
-    
-                                if (tmpUploadsMap[entry] === undefined){
-                                    tmpUploadsMap[entry] = {
-                                        fileCount, 
-                                        lastUpdated: new Date().getTime(),
-                                        committed: false
-                                    };
-                                }else{
-                                    const prevFileCount = tmpUploadsMap[entry].fileCount;
-                                    stale = !tmpUploadsMap[entry].committed && 
-                                            prevFileCount === fileCount && 
-                                            (new Date().getTime() - tmpUploadsMap[entry].lastUpdated > 1000 * 60 * 60 * staleUploadsTimeout);
+            let stale = false;
+            let tmpPath = path.join(TMP_ROOT, entry);
+            logger.debug(`[TAPIS DEBUG] Checking directory for cleanup: ${tmpPath}`);
 
-                                    // Update if the count has changed
-                                    if (prevFileCount !== fileCount){
-                                        tmpUploadsMap[entry].fileCount = fileCount;
-                                        tmpUploadsMap[entry].lastUpdated = new Date().getTime();
-                                    }
-                                }
-                            }catch(e){
-                                logger.error(e);
-                            }
-                        }
-                        
-                        // This is async, it will not block!
-                        fs.stat(tmpPath, function(err, stats){
-                            if (err) logger.error(err);
-                            else{
-                                const mtime = new Date(stats.mtime);
-                                if (stale || (new Date().getTime() - mtime.getTime() > 1000 * 60 * 60 * 48)){
-                                    logger.info("Cleaning up " + entry + " " + (stale ? "[stale]" : ""));
-                                    self.rmfr(tmpPath, err => {
-                                        if (err) logger.error(err);
-                                    });
-                                    delete (tmpUploadsMap[entry]);
-                                }
-                            }
-                        });
+            if (global.taskProcessingDirs?.has(tmpPath)) {
+                logger.info(`[TAPIS DEBUG] Skipping cleanup of ${tmpPath} - task still marked as processing`);
+                continue;
+            }
+
+            if (await hasNfsTempFiles(tmpPath)) {
+                if (shouldLogCleanupFailure(tmpPath)) {
+                    logger.warn(`[TAPIS DEBUG] Skipping cleanup of ${tmpPath} - NFS .nfs* files are still open by another process`);
+                }
+                continue;
+            }
+
+            if (staleUploadsTimeout > 0){
+                try{
+                    // Check if directory has a Tapis upload lock file
+                    const lockFile = path.join(tmpPath, '.tapis_upload_in_progress');
+                    if (fs.existsSync(lockFile)) {
+                        logger.info(`[TAPIS DEBUG] Skipping cleanup of ${tmpPath} - Tapis upload in progress`);
+                        continue;
                     }
                     
-                    // Remove entries in the upload map that aren't in tmp dir
-                    // to avoid memory leaks
-                    for (let entry of Object.keys(tmpUploadsMap)){
-                        if (entries.indexOf(entry) === -1){
-                            delete (tmpUploadsMap[entry]);
+                    const fileCount = await self.filesCount(tmpPath);
+
+                    if (tmpUploadsMap[entry] === undefined){
+                        tmpUploadsMap[entry] = {
+                            fileCount, 
+                            lastUpdated: new Date().getTime(),
+                            committed: false
+                        };
+                    }else{
+                        const prevFileCount = tmpUploadsMap[entry].fileCount;
+                        stale = !tmpUploadsMap[entry].committed && 
+                                prevFileCount === fileCount && 
+                                (new Date().getTime() - tmpUploadsMap[entry].lastUpdated > 1000 * 60 * 60 * staleUploadsTimeout);
+
+                        // Update if the count has changed
+                        if (prevFileCount !== fileCount){
+                            tmpUploadsMap[entry].fileCount = fileCount;
+                            tmpUploadsMap[entry].lastUpdated = new Date().getTime();
                         }
                     }
-
-                    resolve();
+                }catch(e){
+                    logger.error(e);
                 }
-            });
-        })
+            }
+            
+            try {
+                const stats = await fs.promises.stat(tmpPath);
+                const mtime = new Date(stats.mtime);
+                if (stale || (new Date().getTime() - mtime.getTime() > 1000 * 60 * 60 * 48)){
+                    logger.info("Cleaning up " + entry + " " + (stale ? "[stale]" : ""));
+                    await new Promise(resolve => {
+                        self.rmfr(tmpPath, err => {
+                            if (err) {
+                                const message = err.message || String(err);
+                                const nfsBusy = err.code === 'EBUSY' || message.includes('Device or resource busy') || message.includes('.nfs');
+                                if (nfsBusy) {
+                                    if (shouldLogCleanupFailure(tmpPath)) {
+                                        logger.warn(`[TAPIS DEBUG] Cleanup deferred for ${tmpPath} - NFS file still busy`);
+                                    }
+                                } else {
+                                    logger.error(err);
+                                }
+                            } else {
+                                delete tmpCleanupFailures[tmpPath];
+                            }
+                            resolve();
+                        });
+                    });
+                    delete (tmpUploadsMap[entry]);
+                }
+            } catch (err) {
+                if (err.code !== 'ENOENT') logger.error(err);
+            }
+        }
+        
+        // Remove entries in the upload map that aren't in tmp dir
+        // to avoid memory leaks
+        for (let entry of Object.keys(tmpUploadsMap)){
+            if (entries.indexOf(entry) === -1){
+                delete (tmpUploadsMap[entry]);
+            }
+        }
     },
 
     markTaskAsCommitted: function(taskId){
@@ -206,12 +249,10 @@ module.exports = {
 
     // rm -fr implementation. dir is not checked, so this could wipe out your system.
     rmfr: function(dir, cb){
-        if (['darwin', 'linux', 'freebsd'].indexOf(os.platform()) !== -1){
-            // Rimraf leaks on Linux, use faster/better rm -fr
-            return child_process.exec(`rm -rf ${dir}`, cb);
-        }else{
-            return rimraf(dir, cb);
+        if (fs.rm) {
+            return fs.rm(dir, { recursive: true, force: true }, cb);
         }
+        return rimraf(dir, cb);
     },
 
     // JSON helper for responses
